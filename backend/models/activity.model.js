014a8c56838f88b8f49d63baefcd5d4c3fd9e54c
Prince9g <142648202+Prince9g@users.noreply.github.com>
2025-04-28 08:57:34 +0530

    added

--- backend/models/activity.model.js
+++ backend/models/activity.model.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const roomActivitySchema = new mongoose.Schema({
+  socketId: { type: String, required: true },
   roomId: { type: String, required: true },
+  userIP: { type: String },
   joinedAt: { type: Date, default: Date.now },
   leftAt: { type: Date },
-  userIP: { type: String },
 });
 
-export default mongoose.model("RoomActivity", roomActivitySchema);
+const RoomActivity = mongoose.model("RoomActivity", roomActivitySchema);
+
+export default RoomActivity;
